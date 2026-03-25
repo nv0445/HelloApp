846e7c87cc6911f5c0015e2da1e27958fba510ea
Niharika <nv0445@srmist.edu.in>
2026-03-25 10:47:00 +0530

    UC4: Display Hello with multiple command-line arguments

--- Hello.java
+++ Hello.java
@@ -1,9 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
 
-        // Check if user provided a name
+        // If names are provided
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+
+            System.out.print("Hello ");
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+
+                // Add space between names
+                if (i < args.length - 1) {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
         } 
         // Default case
         else {
