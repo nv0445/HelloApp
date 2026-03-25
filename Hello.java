6c263d0c2bf39e3d0186a6828721afff0ab31950
Niharika <nv0445@srmist.edu.in>
2026-03-25 10:51:47 +0530

    UC5: Display Hello using enhanced for loop with default message

--- Hello.java
+++ Hello.java
@@ -1,25 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
 
-        // If names are provided
+        // If arguments are provided
         if (args.length > 0) {
 
             System.out.print("Hello ");
 
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-
-                // Add space between names
-                if (i < args.length - 1) {
-                    System.out.print(" ");
-                }
+            // Enhanced for loop
+            for (String name : args) {
+                System.out.print(name + " ");
             }
 
             System.out.println();
         } 
         // Default case
         else {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         }
 
     }
